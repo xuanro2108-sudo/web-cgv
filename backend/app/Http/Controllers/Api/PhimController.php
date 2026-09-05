a7f65049ddd1e8582bd229a2448f1140f7c3f669
Truongdoan-02 <doantruong202@gmail.com>
2026-09-06 02:03:01 +0700

    feat: fix PhimController

--- backend/app/Http/Controllers/Api/PhimController.php
+++ backend/app/Http/Controllers/Api/PhimController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Phim;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class PhimController extends Controller
 {
